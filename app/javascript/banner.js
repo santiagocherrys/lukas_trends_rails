cfc02286b2736a581904e861a0503b614962c117
darwingx990 <darwingx990@hotmail.com>
2025-01-30 14:54:26 -0500

    feat: Partial banner and advertisement controller were created.

---
+++ app/javascript/banner.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function() {
+    function refreshBanner() {
+      fetch('/banner')
+        .then(response => response.text())
+        .then(html => {
+          let bannerContainer = document.querySelector(".ad-banner");
+          if (bannerContainer) {
+            bannerContainer.outerHTML = html;
+          }
+        });
+    }
+  
+    setInterval(refreshBanner, 5 * 60 * 1000); // Cambia cada 5 minutos
+  });
+  
