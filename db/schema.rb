8bbe187a863808a2e2dbd56447de9f315b90eca1
darwingx990 <darwingx990@hotmail.com>
2025-01-30 07:57:59 -0500

    feat: add advertisement model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_01_15_185745) do
+ActiveRecord::Schema[7.2].define(version: 2025_01_23_051111) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "advertisements", force: :cascade do |t|
+    t.string "title"
+    t.string "image_url"
+    t.string "link"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "chats", force: :cascade do |t|
     t.bigint "user_id", null: false
