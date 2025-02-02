aa20ebcda66a405904ddf9e50a2a0a7ede071d96
daguttt <dagutmu667@gmail.com>
2025-02-01 20:38:45 -0500

    fix: recover users table schema to 057a4549

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_01_31_180010) do
+ActiveRecord::Schema[7.2].define(version: 2025_01_27_235439) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -53,7 +53,11 @@ ActiveRecord::Schema[7.2].define(version: 2025_01_31_180010) do
     t.string "uid"
     t.string "avatar_url"
     t.string "provider"
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "chats", "users"
